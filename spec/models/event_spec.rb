c178df4e99af6da25c314b9c80773c0a81057b2d
Gabriel Kay <gabrielkay1@gmail.com>
2019-06-27 12:57:03 -0400

    added validation tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:restaurant)}
+  it { should validate_presence_of(:city)}
+  it { should validate_presence_of(:start_time)}
 end
