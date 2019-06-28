c300a6f5703ea25a476e50c610db6a0a06152800
Gabriel Kay <37525354+gabrielkay@users.noreply.github.com>
2019-06-28 16:24:16 -0400

    Events Controller, Routing (#6)
    * generated controller
    * edit/update works, removed unused views
    * made fields required in html of event forms

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,50 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.where(city: current_user.location)
+    # (user/events index will be index of the user's events that they've joined)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    # Will have a message board once comments are added
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.city = current_user.location
+    @event.creator_id = current_user.id
+    if (@event.save)
+      redirect_to events_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+    # Will need to be restricted to the event creator
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to events_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private def event_params
+    params.require(:event).permit(:restaurant, :min_people, :max_people, :start_time, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :events
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
