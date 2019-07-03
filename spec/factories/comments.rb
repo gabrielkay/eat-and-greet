c07a5f4e2d45efdc1e8301d06694b32425fad4a5
Gabriel Kay <gabrielkay1@gmail.com>
2019-07-03 14:29:25 -0400

    added tests

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    user
+    event
+    body { "Sample Comment "}
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    subject { build(:comment) }
+
+    it { should belong_to(:user) }
+    it { should belong_to(:event) }
+  end
+
+  context 'empty comment' do
+    let(:comment) { build(:comment, body: nil) }
+
+    it { should validate_presence_of(:body) }
+  end
 end
