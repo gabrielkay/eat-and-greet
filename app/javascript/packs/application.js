b8aa920889583211f87bf2022b9d06d06212368d
Amy Townsend <amygtownsend@gmail.com>
2019-07-05 15:09:37 -0400

    add event card styling and set up scss file structure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import '../src/normalize.scss';
 import '../src/application.scss';
 
 console.log('Hello World from Webpacker');
