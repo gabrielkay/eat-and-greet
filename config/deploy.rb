b039345c4600d6b7e1fe83fb0b60cbaf7378fc4c
Chris Jones <chris.jones@viget.com>
2019-07-08 09:12:27 -0400

    Set up production deploy (#27)

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+require 'viget/deployment/rails'
+
+set :application,    'eatandgreet'
+set :group_writable, false
+
+set :repository, 'git@github.com:vigetlabs/eat-and-greet.git'
+set :branch,     'master'
+
+set :user, 'deploy'
+
+set :slack_url,     'https://hooks.slack.com/services/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'
+set :slack_channel, '#interns-19'
+set :slack_emoji,   ':fork_and_knife:'
+
+namespace :deploy do
+  namespace :configuration do
+    desc "Link created configuration to current release"
+    task :symlink, except: { no_release: true } do
+      run %{rm #{release_path}/config/database.yml}
+      run %{ln -nsf "#{shared_path}/config/database.yml" "#{release_path}/config/database.yml"}
+
+      run %{ln -nsf "#{shared_path}/config/master.key" "#{release_path}/config/master.key"}
+    end
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,3 @@
+set :slack_app_url, 'https://eat-and-greet.production.vigetx.com'
+
+server '138.197.1.166', :web, :app, :db, primary: true
