3959deb4e4194d15aacb9884b19079ac166f7056
Gabriel Kay <gabrielkay1@gmail.com>
2019-07-08 10:04:54 -0400

    changed order of validations in model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-  validates :body, presence: true
-  validates :user_id, presence: true
-  validates :event_id, presence: true
-
   belongs_to :user
   belongs_to :event
   belongs_to :comment, optional: true
   has_many :comments
+
+  validates :body, presence: true
+  validates :user_id, presence: true
+  validates :event_id, presence: true
 end
