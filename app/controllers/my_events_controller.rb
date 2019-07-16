0608728532040b6e40a8bff1d1c2c2b0d537eab5
Amy Townsend <amygtownsend@gmail.com>
2019-07-16 13:47:50 -0400

    remove accidental change to my_events controller

--- app/controllers/my_events_controller.rb
+++ app/controllers/my_events_controller.rb
@@ -2,17 +2,4 @@ class MyEventsController < ApplicationController
   def index
     @events = current_user.events_as_member
   end
-
-  def edit
-    @event = Event.find(params[:id])
-    authorize @event
-    # Will need to be restricted to the event creator
-  end
-
-  def destroy
-    @event = Event.find(params[:id])
-    authorize @event
-    @event.destroy
-    redirect_to events_path
-  end
 end
