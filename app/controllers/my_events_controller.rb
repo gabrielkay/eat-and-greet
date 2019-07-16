ded23580a4a9e013c9a98a43e7e5aee897413d32
Amy Townsend <amygtownsend@gmail.com>
2019-07-16 13:41:12 -0400

    restrict edit/cancel/leave table actions on card view

--- app/controllers/my_events_controller.rb
+++ app/controllers/my_events_controller.rb
@@ -2,4 +2,17 @@ class MyEventsController < ApplicationController
   def index
     @events = current_user.events_as_member
   end
+
+  def edit
+    @event = Event.find(params[:id])
+    authorize @event
+    # Will need to be restricted to the event creator
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    authorize @event
+    @event.destroy
+    redirect_to events_path
+  end
 end
