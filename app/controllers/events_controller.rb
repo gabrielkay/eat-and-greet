b7415d17cd9aa586730a4fc3b21ae4469969d5a7
Gabriel Kay <gabrielkay1@gmail.com>
2019-07-24 16:42:24 -0400

    error checking in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,10 +21,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.build_with_member(current_user, event_params)
-    convert_to_datetime_and_assign(event_params)
+    @event = convert_to_datetime_and_assign(@event, event_params)
     if (@event.save)
       redirect_to my_tables_path
     else
+      flash_errors(@event.errors)
       render 'new'
     end
   end
@@ -36,11 +37,12 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    convert_to_datetime_and_assign(event_params)
+    @event = convert_to_datetime_and_assign(@event, event_params)
     authorize @event
     if @event.update(event_params)
       redirect_to my_tables_path
     else
+      flash_errors(@event.errors)
       render 'edit'
     end
   end
@@ -52,12 +54,16 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
-  def convert_to_datetime_and_assign(params)
-    @date_field = Date.parse(params[:date_field]).strftime("%Y-%m-%d")
-    @start_time_field = Time.parse(params[:start_time_field]).strftime("%H:%M:%S")
-    @end_time_field = Time.parse(params[:end_time_field]).strftime("%H:%M:%S")
-    @event.start_time = DateTime.parse("#{@date_field} #{@start_time_field}")
-    @event.end_time = DateTime.parse("#{@date_field} #{@end_time_field}")
+  def convert_to_datetime_and_assign(event, params)
+    date_field = Date.parse(params[:date_field]).strftime("%Y-%m-%d")
+    start_time_field = Time.parse(params[:start_time_field]).strftime("%H:%M:%S")
+    end_time_field = Time.parse(params[:end_time_field]).strftime("%H:%M:%S")
+    event.start_time = DateTime.parse("#{date_field} #{start_time_field}")
+    event.end_time = DateTime.parse("#{date_field} #{end_time_field}")
+    event
+    rescue ArgumentError
+      event.errors.add(:start_time, :invalid, message: "Date or time was invalid")
+      event
   end
 
   private def event_params
